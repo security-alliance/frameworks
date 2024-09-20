08414ebcef204e8e2ec77d68564dc16b2d6c646d
Jorge de los Santos <tebayoso@gmail.com>
2024-09-20 14:29:52 -0300

    Modified serve.sh script to be compatible with macos The Serve SH Script needed to work in any oss. This commit solves that.

--- serve.sh
+++ serve.sh
@@ -3,6 +3,22 @@ if ! test -e theme/index.hbs; then
     echo "Run serve.sh inside root folder."
     exit 1
 fi
-trap "sed -i 's|{{ path_to_root }}|/book/|g' theme/index.hbs" INT
-sed -i 's|/book/|{{ path_to_root }}|g' theme/index.hbs
-mdbook serve
+
+# Function to perform sed operation in a cross-platform manner
+cross_platform_sed() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # macOS (BSD sed)
+        sed -i '' 's|/book/|{{ path_to_root }}|g' theme/index.hbs
+    else
+        # Linux and others (GNU sed)
+        sed -i 's|/book/|{{ path_to_root }}|g' theme/index.hbs
+    fi
+}
+
+# Set up trap for cleanup
+trap "cross_platform_sed" INT
+
+# Perform initial sed operation
+cross_platform_sed
+
+mdbook serve --watcher native
