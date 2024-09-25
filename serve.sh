de4cbc4049feb6724974939659f83e471466f25a
matta <matt@theredguild.org>
2024-09-25 15:09:15 -0300

    Deprecating previous replacement with sed on serve.sh

--- serve.sh
+++ serve.sh
@@ -4,25 +4,6 @@ if ! test -e theme/index.hbs; then
     exit 1
 fi
 
-
-# Function to perform sed operation in a cross-platform manner
-cross_platform_sed() {
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-        # macOS (BSD sed)
-        trap "sed -i '' 's|{{ path_to_root }}logo\.svg|/book/logo\.svg|g' theme/index.hbs" INT
-        sed -i '' 's|/book/logo\.svg|{{ path_to_root }}logo\.svg|g' theme/index.hbs
-    else
-        # Linux and others (GNU sed)
-        trap "sed -i 's|{{ path_to_root }}logo\.svg|/book/logo\.svg|g' theme/index.hbs" INT
-        sed -i 's|/book/logo\.svg|{{ path_to_root }}logo\.svg|g' theme/index.hbs
-    fi
-}
-
-
-
-# Perform initial sed operation
-cross_platform_sed
-
 # Regenerate tags
 node theme/tags_generator.js
 
