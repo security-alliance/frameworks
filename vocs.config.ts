3b6f89f39f5a04e6c01c289c08eacfd4c10273fa
Chirag <chiragkcv2020@gmail.com>
2025-10-13 15:05:06 +0530

    chore(dev): pin Node 20 via .nvmrc, engines; update CONTRIBUTING for local docs build

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vocs'
 
 const config = {
+  outDir: 'docs/dist',
   banner: {
     content: '***This is a work in progress and not a release. We are looking for volunteers. See [Issues](https://github.com/security-alliance/frameworks/issues) and [Contribution](https://github.com/security-alliance/frameworks/blob/develop/docs/pages/contribute/contributing.mdx) to know how to collaborate.***',
     height: '30px',
