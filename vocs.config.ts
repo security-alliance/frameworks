a2b6ac61273b9f6d672c6412c032e80b5b4ecc0f
Robert MacWha <trebor.ahwcam@gmail.com>
2025-10-21 09:55:44 -0400

    update page name

--- vocs.config.ts
+++ vocs.config.ts
@@ -305,7 +305,7 @@ const config = {
             { text: 'Code Reviews and Peer Audits', link: '/secure-software-development/code-reviews-peer-audits', dev: true },
             { text: 'Secure Code Repositories and Version Control', link: '/secure-software-development/secure-code-repositories-version-control', dev: true },
             { text: 'Threat Modeling and Secure Design Principles', link: '/secure-software-development/threat-modeling-secure-design-principles', dev: true },
-            { text: 'Devcontainers', link: '/secure-software-development/secure-development-environments', dev: true },
+            { text: 'Devcontainers & Secure Development Environments', link: '/secure-software-development/secure-development-environments', dev: true },
             { text: 'Devcontainer Security', link: '/secure-software-development/devcontainer-security', dev: true },
           ]
         },
