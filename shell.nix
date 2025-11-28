403c78ba5afe242ec2f4ade2b417dc0b9f52bdf6
Robert MacWha <github@macwha.com>
2025-11-28 14:49:07 -0500

    remove unused deps from shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,9 @@
-{ pkgs ? import <nixpkgs> { } }:
-
+{
+  pkgs ? import <nixpkgs> { },
+}:
 pkgs.mkShell {
   packages = with pkgs; [
     pnpm
     nodejs_22
-    python313
-    python313Packages.pyyaml
-    python313Packages.openai
   ];
 }
