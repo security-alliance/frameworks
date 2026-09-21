0cdbfcc785bd906fb2430e4510e9d98e4d1ce0bc
Sara Russo <sararusso984@gmail.com>
2026-09-21 13:22:45 +0200

    Chore: update license copyright year to 2026 (#634)

--- docs/pages/_slots.tsx
+++ docs/pages/_slots.tsx
@@ -3,7 +3,7 @@ import { TagFilter } from '../../components'
 export function Footer() {
   return (
     <div className="footer">
-      <div>Security Frameworks © 2023 by Security Alliance, licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="license noopener noreferrer">CC BY-SA 4.0</a>.</div>
+      <div>Security Frameworks © 2026 by Security Alliance, licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank" rel="license noopener noreferrer">CC BY-SA 4.0</a>.</div>
     </div>
   )
 }
